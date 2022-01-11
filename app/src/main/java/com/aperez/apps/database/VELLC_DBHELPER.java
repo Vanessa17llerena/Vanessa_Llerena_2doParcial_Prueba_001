1ca54b0ccb12325831ce83b3f02d6df21ef6001e
Vanessa Llerena <vanestefania01@gmail.com>
2022-01-10 19:44:35 -0500

    COMIIT 19:4

--- app/src/main/java/com/aperez/apps/database/VELLC_DBHELPER.java
+++ app/src/main/java/com/aperez/apps/database/VELLC_DBHELPER.java
@@ -1,4 +1,14 @@
 package com.aperez.apps.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class VELLC_DBHELPER {
+    private String createTable_Usuarios ="CREATE TABLE Usuarios("+
+            "Codigo INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            "Nombre TEXT,"+
+            "Contraseña TEXT";
+
+    }
+    }
 }
