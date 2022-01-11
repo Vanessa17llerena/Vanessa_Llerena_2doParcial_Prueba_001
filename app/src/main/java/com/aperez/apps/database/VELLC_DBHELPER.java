2252005f3ea6053962589a4bf873269da782f242
Vanessa Llerena <vanestefania01@gmail.com>
2022-01-10 19:46:32 -0500

    COMIIT 19:45

--- app/src/main/java/com/aperez/apps/database/VELLC_DBHELPER.java
+++ app/src/main/java/com/aperez/apps/database/VELLC_DBHELPER.java
@@ -2,13 +2,28 @@ package com.aperez.apps.database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
 
-public class VELLC_DBHELPER {
+public class VELLC_DBHELPER  extends SQLiteOpenHelper {
     private String createTable_Usuarios ="CREATE TABLE Usuarios("+
             "Codigo INTEGER PRIMARY KEY AUTOINCREMENT,"+
             "Nombre TEXT,"+
             "Contraseña TEXT";
 
+    public VELLC_DBHELPER(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
     }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTable_Usuarios);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 }
+
