d28dd67d0f55d23a3a9147b3815b09b4fc629803
Vanessa Llerena <vanestefania01@gmail.com>
2022-01-10 20:25:30 -0500

    COMIIT 8:25

--- app/src/main/java/com/aperez/apps/androidfunwithflags/VELLC_Registro.java
+++ app/src/main/java/com/aperez/apps/androidfunwithflags/VELLC_Registro.java
@@ -1,13 +1,34 @@
 package com.aperez.apps.androidfunwithflags;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Fragment;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
-public class VELLC_Registro extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vellc_registro);
+public class VELLC_Registro extends Fragment {
+    private final View.OnClickListener saveContactButtonClicked =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // hide the virtual keyboard
+                    ((InputMethodManager) getActivity().getSystemService(
+                            Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                            getView().getWindowToken(), 0);
+                    saveContact(); // save contact to the database
+                }
+
+                private void saveContact() {
+                    ContentValues contentValues = new ContentValues();
+
+                }
+            };
+
+
     }
-}
