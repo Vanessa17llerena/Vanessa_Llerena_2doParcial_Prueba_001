d3025135259f2841ac878a2d3c7e4e32457fec1a
Vanessa Llerena <vanestefania01@gmail.com>
2022-01-10 19:57:25 -0500

    COMIIT 8:00

--- app/src/main/java/com/aperez/apps/androidfunwithflags/VELLC_LOGIN.java
+++ app/src/main/java/com/aperez/apps/androidfunwithflags/VELLC_LOGIN.java
@@ -2,12 +2,31 @@ package com.aperez.apps.androidfunwithflags;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class VELLC_LOGIN extends AppCompatActivity {
+    private EditText editTextUsuario;
+    private EditText editTextContraseña;
+    private Button buttonIngresar;
+    private Button buttonRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vellc_login);
+        editTextUsuario= findViewById(R.id.editTextUsuario);
+        editTextContraseña= findViewById(R.id.editTextContraseña);
+        buttonIngresar = findViewById(R.id.buttonIngresar);
+        buttonRegistrar= findViewById(R.id.buttonRegistrar);
+        buttonIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = editTextUsuario.getText().toString();
+                String contraseña= editTextContraseña.getText().toString();
+            }
+        });
     }
+
 }
